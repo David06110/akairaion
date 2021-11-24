07efb28734cb5fe41f221a809994ec7bc8eb3cc4
davidhaffner <david.haffner06@gmail.com>
2021-11-24 17:05:58 +0100

    added new croix

--- app/controllers/croix_controller.rb
+++ app/controllers/croix_controller.rb
@@ -1,6 +1,10 @@
 class CroixController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  skip_before_action :authenticate_user!, only: %i[index new]
   def index
+
+  end
+
+  def new
 
   end
 end
